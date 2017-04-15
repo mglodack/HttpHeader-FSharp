a3cb03aaae45ee5ac6e6918984aa18d4ca71602d
Mathew Glodack <glodack@gmail.com>
2017-04-15 16:05:33 -0700

    Add MSBuild, CI and Clean targets

--- build.fsx
+++ build.fsx
@@ -3,11 +3,21 @@
 open Fake
 open Fake.Testing.XUnit2
 
-Target "Hello" (fun _ -> printfn "Hello World")
+Target "Clean" (fun _ -> printfn "Clean output here.")
 
 Target "Test" (fun _ ->
   !! (@"**\*\bin\**\*Tests.dll")
   |> xUnit2 id
 )
+
+Target "MSBuild" (fun _ ->
+  !! ("*.sln")
+  |> MSBuildRelease "bin" "Build"
+  |> ignore
+)
+
+Target "CI" id
+
+"CI" <== [ "Test"; "MSBuild" ]
 
 RunTargetOrDefault "Hello"
