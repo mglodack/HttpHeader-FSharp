153de623f7b096bac0273d9b415f3add63fca207
Mathew Glodack <glodack@gmail.com>
2017-04-15 15:57:01 -0700

    Create xUnit test target

--- build.fsx
+++ build.fsx
@@ -1,7 +1,13 @@
 #r @"._fake\packages\FAKE\tools\FakeLib.dll"
 
 open Fake
+open Fake.Testing.XUnit2
 
 Target "Hello" (fun _ -> printfn "Hello World")
+
+Target "Test" (fun _ ->
+  !! (@"**\*\bin\**\*Tests.dll")
+  |> xUnit2 id
+)
 
 RunTargetOrDefault "Hello"
