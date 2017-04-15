fe190084fcad7d47b420993717b6cf1a731844ec
Mathew Glodack <glodack@gmail.com>
2017-04-15 11:42:07 -0700

    Create first failing test

--- tests/HttpHeader.Tests/LinkParserTests.fs
+++ tests/HttpHeader.Tests/LinkParserTests.fs
@@ -1,7 +1,23 @@
 ﻿module LinkParserTests
 
 open Xunit
+open HttpHeader
 
 [<Fact>]
-let ``this should pass``() = Assert.False(false)
+let ``parse url and next should return correct url and direction``() =
+  let toParse = "<https://api.example.foobar.com/this/path/is/cool>; rel=\"next\""
+
+  let expectedUrl = "https://api.example.foobar.com/this/path/is/cool"
+  let expectedDirection = LinkParser.LinkDirection.Next
+
+  let result = LinkParser.Parse toParse
+  let onlySomeResult = Array.choose id result
+  let (url, direction) = Array.head onlySomeResult
+
+
+  Assert.NotEmpty result
+  Assert.NotEmpty onlySomeResult
+  Assert.Equal(1, (Array.length onlySomeResult))
+  Assert.Equal(expectedUrl, url)
+  Assert.Equal(expectedDirection, direction)
 
